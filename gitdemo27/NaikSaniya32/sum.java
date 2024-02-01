75f168e672ae0ad69c963651141a9c9c394d4f25
NaikSaniya32 <157671829+NaikSaniya32@users.noreply.github.com>
2024-02-01 12:18:24 +0530

    Add files via upload

---
+++ gitdemo27/NaikSaniya32/sum.java
@@ -0,0 +1,11 @@
+ public class sumofnos
+ { 
+    public static void main(string args[])
+    {
+        int num1=4, num2=6, sum;
+        sum=num1+num2;
+        System.out.println("The sum of the numbers is:" +sum);
+        System.out.println("Display num1");
+    }
+ } 
+ 
